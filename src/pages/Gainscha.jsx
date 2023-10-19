7450ba2f67d1d8ec6258efb78211f1a029d5c9f6
Briggette Echeverría <briggette_1110@hotmail.com>
2023-10-18 21:48:19 -0500

    gainscha

--- src/pages/Gainscha.jsx
+++ src/pages/Gainscha.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import logo from '../images/LogoUnis.png'
 import GI2408T from '../images/GI-2408T.png'
 import GA2408T from '../images/GA-2408T.png'
+import axios from "axios";
 
 const Gainscha = () => {
+
+  const handleNacional = async e => {
+    e.preventDefault()
+    const nombrehost = 'HOST_GAINSCHA_1'
+    const { data } = await axios.post(`http://192.168.0.132:3001/print`, {nombrehost})
+    console.log('HandleNacional');
+  }
+  const handleInternacional = e => {
+    e.preventDefault()
+
+    console.log('HandleINterNacional');
+  }
+
   return (
     <>
       <div className='flex h-[10vh] p-2 place-items-center'>
@@ -14,13 +28,17 @@ const Gainscha = () => {
       <div className='grid grid-cols-2 text-center p-10 font-bold uppercase text-3xl  place-items-center'>
         <p>Envios Nacionales</p>
         <p>Envios Internacionales</p>
-        <div className='bg-sky-700 py-3 mt-2 h-44 text-white uppercase font-bold rounded hover:cursor-pointer
+        <div
+          onClick={handleNacional}
+          className='bg-sky-700 py-3 mt-2 h-80 text-white uppercase font-bold rounded hover:cursor-pointer
                             hover:bg-sky-800 transition-colors disabled:bg-gray-400 w-2/3 flex place-items-center justify-center'>
-          <img src={GI2408T} className=' w-2/3'  />
+          <img src={GI2408T} className=' w-2/3' />
         </div>
-        <div className='bg-sky-700 py-3 mt-2 h-80 text-white uppercase font-bold rounded hover:cursor-pointer
+        <div
+        onClick={handleInternacional} 
+        className='bg-sky-700 py-3 mt-2 h-80 text-white uppercase font-bold rounded hover:cursor-pointer
                             hover:bg-sky-800 transition-colors disabled:bg-gray-400 w-2/3 flex place-items-center justify-center' >
-          <img src={GA2408T} className=' w-2/3'  />
+          <img src={GA2408T} className=' w-2/3' />
         </div>
 
 
